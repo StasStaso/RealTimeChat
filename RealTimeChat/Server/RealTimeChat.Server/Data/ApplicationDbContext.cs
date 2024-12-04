f1d1b6334f4edbf2e42c10b3e309acb10be3d738
Staso Stanislav <xctacx5@gmail.com>
2024-12-04 10:05:25 +0200

    Add AplicationDbContext

---
+++ RealTimeChat/Server/RealTimeChat.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealTimeChat.Server.Data.Entities;
+
+namespace RealTimeChat.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Message>(e =>
+            {
+                e.HasOne(m => m.ToUser).WithMany().OnDelete(DeleteBehavior.NoAction);
+                e.HasOne(m => m.FromUser).WithMany().OnDelete(DeleteBehavior.NoAction);
+            });
+        }
+    }
+}
