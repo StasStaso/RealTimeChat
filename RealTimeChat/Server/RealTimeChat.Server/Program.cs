b0340a8bfa6427f25bae5f916da653d098d213e9
Staso Stanislav <xctacx5@gmail.com>
2024-12-04 12:33:01 +0200

    Create Error pages

--- RealTimeChat/Server/RealTimeChat.Server/Program.cs
+++ RealTimeChat/Server/RealTimeChat.Server/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using RealTimeChat.Server;
 using RealTimeChat.Server.Data;
+using RealTimeChat.Server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -49,14 +50,28 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapHub<RealTimeChatHub>("/hubs/realtimechat");
+app.MapFallbackToFile("index.html");
 
 app.Run();
