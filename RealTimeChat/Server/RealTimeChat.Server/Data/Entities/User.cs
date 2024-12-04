fdc1d63d32949db08fe9950e9c31e702416bb1c2
Staso Stanislav <xctacx5@gmail.com>
2024-12-04 09:51:30 +0200

    Add Entities Message and User

---
+++ RealTimeChat/Server/RealTimeChat.Server/Data/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealTimeChat.Server.Data.Entities
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(25)]
+        public string Name { get; set; }
+
+        public DateTime AddedOn { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Password { get; set; }
+    }
+}
