aea2c9f68abfbf3fe3b0243cc6e72420c0380c6e
Staso Stanislav <xctacx5@gmail.com>
2024-12-04 11:29:43 +0200

    Add UserController

---
+++ RealTimeChat/Server/RealTimeChat.Server/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealTimeChat.Server.Data;
+using RealTimeChat.Shared.DTOs;
+
+namespace RealTimeChat.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : BaseController
+    {
+        private readonly ApplicationDbContext _chatContext;
+
+        public UserController(ApplicationDbContext chatContext)
+        {
+            _chatContext = chatContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserDto>> GetUsers() 
+        {
+            return await _chatContext.Users
+                .AsNoTracking()
+                .Where(u => u.Id != UserId)
+                .Select(u => new UserDto(u.Id, u.Name, false))
+                .ToListAsync();
+        }
+
+        [HttpGet("chats")]
+        public async Task<IEnumerable<UserDto>> GetUserChats
+            (CancellationToken cancellationToken) 
+        {
+            IEnumerable<UserDto> chatUsers = new List<UserDto>();
+
+            var uniqueUsers = await _chatContext.Messages
+                .AsNoTracking()
+                .Where(m => m.FromId == UserId || m.ToId == UserId)
+                .Select(m => new { From = m.FromId, To = m.ToId })
+                .Distinct()
+                .ToListAsync(cancellationToken);
+            
+            var uniqueUserIds = new HashSet<int>();
+            uniqueUsers.ForEach(u =>
+            {
+                if (u.From != UserId)
+                    uniqueUserIds.Add(u.From);
+                if (u.To != UserId)
+                    uniqueUserIds.Add(u.To);
+            });
+            if (uniqueUserIds.Count > 0)
+            {
+                chatUsers = await _chatContext.Users
+                                    .AsNoTracking()
+                                    .Where(u => uniqueUserIds.Contains(u.Id))
+                                    .Select(u => new UserDto(u.Id, u.Name, false))
+                                    .ToListAsync(cancellationToken);
+            }
+            return chatUsers;
+        }
+    }
+}
